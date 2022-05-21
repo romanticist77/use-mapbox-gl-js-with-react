a4cc0bfb7e4665425d2345b91a2504217c8e2a2f
Kamran Akhmedov <romanticist77@gmail.com>
2022-05-21 13:09:00 +0100

    created a new react app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useEffect, useState } from 'react';
+import mapboxgl from 'mapbox-gl';
 
 function App() {
+  const [random, setRandom] = useState(Math.random());
+
+  useEffect(() => {
+    const map = new mapboxgl.Map({
+      container: "map",
+      style: "mapbox://styles/mapbox/dark-v10",
+      center: [37.61192, 55.76199],
+      zoom: 10
+    });
+  }, []);
+
+  mapboxgl.accessToken =
+    "pk.eyJ1IjoiYWNobWVkb3ciLCJhIjoiY2wzOGY4aDdqMDBodzNqcHIwZTF2Y3F3MyJ9.2Cspq7xwjY058eiwiJpdAg";
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button
+        className="ui-button"
+        id="rerender"
+        onClick={() => setRandom(Math.random())}
+      >
+        Ререндер!
+      </button>
+      <div id="map"></div>
+    </>
   );
 }
 
