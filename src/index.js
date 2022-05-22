e4b3b2f1194ecc006a26dc66c59c2443de46ed83
Kamran Akhmedov <romanticist77@gmail.com>
2022-05-22 10:50:51 +0100

    changed react dom back

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import './mapbox-gl.css';
 import './index.css';
 import App from './App';
  
 
-const container = document.getElementById('root');
-const root = createRoot(container);
-root.render(<App tab="home" />);
+ReactDOM.render(
+    <App />,
+  document.getElementById('root')
+);
