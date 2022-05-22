606b5be8cca199d7e970d08345b0e7ab6e2a2787
Kamran Akhmedov <romanticist77@gmail.com>
2022-05-22 11:34:57 +0100

    completed disabling eslint

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useRef, useEffect, useState } from 'react';
-import mapboxgl from '!mapbox-gl';
+import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
 
 function App() {
   const [random, setRandom] = useState(Math.random());
